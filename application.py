5175548389d0501b17296deaa33552cf115a5cea
Lesley Cheung <lesleyc@bu.edu>
2019-04-21 15:58:47 -0400

    Made bootstrap folder

--- application.py
+++ application.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template
-
-
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
+
+def create_app():
+    app = Flask(__name__)
+    Bootstrap(app)
+
+    return app
+
 @app.route("/")
 def index():
     return render_template("index.html")
