d7c77a2234767c1ac7b3b771ea0419e2d1e44fc9
Lesley <lesleyc@bu.edu>
2019-04-23 15:00:01 -0400

    updates

---
+++ philmed.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, flash
+from forms import ContactForm
+app = Flask(__name__)
+app.secret_key = 'development key'
+
+@app.route('/philmed', methods = ['GET', 'POST'])
+def philmed():
+   form = philmed()
+   
+   if request.method == 'POST':
+      if form.validate() == False:
+         flash('All fields are required.')
+         return render_template('philmed.html', form = form)
+      else:
+         return render_template('more.html')
+      elif request.method == 'GET':
+         return render_template('philmed.html', form = form)
+
+if __name__ == '__main__':
+   app.run(debug = True)
